27d2b083c811fe1cfeccafaf0587211f658f454a
Katupputhur Ramprasath <dinesh.katupputhur.ramprasath@sap.com>
2021-06-21 13:16:22 -0700

    modified

--- providers.tf
+++ providers.tf
@@ -4,15 +4,12 @@
   # v2.x required "features" block
  # 
 #}
-
-
 provider "azurerm" {
 
   features {}
-
-  subscription_id = "${var.azure-subscription-id}"
-  client_id       = "${var.azure-client-id}"
-  client_secret   = "${var.azure-client-secret}"
-  tenant_id       = "${var.azure-tenant-id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
  }
 
